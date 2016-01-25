66036aa1c2b1e9c79f10e44ef242ec72fd0105fb
hetianqi <449165578@qq.com>
2016-01-25 15:55:20 +0800

    order code

--- public/js/config.js
+++
@@ -1,8 +0,0 @@
-
-require.config({
-	baseUrl: '/js/',
-	paths: {
-		'jquery': 'lib/jquery-1.11.3.min',
-		'jqueryMobile': '../jquery.mobile/jquery.mobile-1.4.5.min'
-	}
-});
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
+require.config({
+	baseUrl: '/js/',
+	paths: {
+		'jquery': 'lib/jquery-1.11.3.min',
+		'jqueryMobile': '../jquery.mobile/jquery.mobile-1.4.5.min'
+	}
+});
 
 require(['config'], function (config) {
 	require(['app/' + PAGE_CONFIG.page]);
