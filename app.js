f49762049a7732c0454f21f2dadd9a85337267ef
hetianqi <449165578@qq.com>
2016-01-27 10:39:18 +0800

    debug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var ctrlPath = path.join(__dirname, 'controllers/');
 
 app.set('views', path.join(__dirname, 'views/'));
 app.set('view engine', 'jade');
-console.log(path.join(__dirname, 'public/'))
+
 app.use(express.static(path.join(__dirname, 'public/')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
